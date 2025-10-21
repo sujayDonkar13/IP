3c332f295c024b2e875cea01cdca89c417ac0a32
adrian <adriancorderom@outlook.com>
2025-10-21 16:19:31 +0200

    Added functional scatterplot

--- functions.jl
+++ functions.jl
@@ -8,13 +8,13 @@ function plot_lineplan(coords, lineplan)
                     title="Line Plan Visualization", xlabel="X Coordinate", 
                     ylabel="Y Coordinate")
     
-    for line_idx in 1:num_lines
+    #= for line_idx in 1:num_lines
         line_stops = lineplan[line_idx, :]
         line_stops = filter(x -> x != 0, line_stops)  # Remove zeros (non-stops)
         line_coords = coords[line_stops .+ 1, :]  # +1 for 1-based indexing
         plot!(plt, line_coords[:,1], line_coords[:,2], lw=2, color=colors[line_idx], 
                     label="Line $line_idx")
-    end
+    end=#
     
     display(plt)
 end 
